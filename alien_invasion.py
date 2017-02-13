841bb9f1d4283f6449b35bbbb57b62f81e9e6b7e
zhangbin0git <zhangbin.email@foxmail.com>
2017-02-14 00:47:03 +0800

    set ship

--- alien_invasion.py
+++ alien_invasion.py
@@ -2,12 +2,16 @@
 import sys
 import pygame
 from settings import Settings
+#导入飞船
+from ship import Ship
 def run_game():
     #初始化游戏并创建一个屏幕对象
     pygame.init()
     ai = Settings()
     screen = pygame.display.set_mode((ai.screen_width, ai.screen_height))
     pygame.display.set_caption("Alien Invasion")
+    #创建飞船
+    ship = Ship(screen)
     #设置背景颜色
     # bg_color = (230, 230, 230)
     #start 主循环
@@ -18,6 +22,8 @@ def run_game():
                 sys.exit()
         #每次重绘屏幕
         screen.fill(ai.bg_color)
+        #绘制飞船
+        ship.blitem()
         #让最近绘制的屏幕可见
         pygame.display.flip()
 run_game()
